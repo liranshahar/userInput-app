6026383adcd5c67eec1cc7054d7fd7e167cdb3ab
Liran Shahar <liranshahar4@gmail.com>
2022-06-27 11:05:36 +0300

    Add button component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card"
 import classes from "./AddUser.module.css"
+import Button from "../UI/Button"
 
 
 const AddUser = props => {
@@ -20,10 +21,10 @@ return (
         <input id="username" type="text" />
 
 
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
 
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
 
 
         </form>
